3a4cd386b3d0d22b700fd9b21c675396cc7c861e
EC2 Default User <ec2-user@ip-172-31-41-164.ap-northeast-1.compute.internal>
2024-05-02 05:17:48 +0000

    [Finish]検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,11 +47,11 @@ class User < ApplicationRecord
     if search == "perfect_match"
       @user = User.where("name LIKE?", "#{word}")
     elsif search == "forward_match"
-       @user = User.where("name LIKE?", "#{word}")
+       @user = User.where("name LIKE?", "#{word}%")
     elsif search == "backward_match"
-       @user = User.where("name LIKE?", "#{word}")
+       @user = User.where("name LIKE?", "%#{word}")
     elsif search == "partial_match"
-       @user = User.where("name LIKE?", "#{word}")
+       @user = User.where("name LIKE?", "%#{word}%")
     else
       @user = User.all
     end
