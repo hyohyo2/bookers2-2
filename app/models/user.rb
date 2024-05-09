39c16b5c1e4df2eb1f662ec3e68a4f49273229a1
EC2 Default User <ec2-user@ip-172-31-41-164.ap-northeast-1.compute.internal>
2024-05-09 04:43:03 +0000

    DM機能アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :follower
 
   has_many :group_users, dependent: :destroy
+  
+  # DM機能
+  has_many :chats, dependent: :destroy
+  has_many :user_rooms, dependent: :destroy
+  has_many :rooms, through: :user_rooms
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum:50 }
