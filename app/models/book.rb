f6560e823d25ffebbaa71a9e0adea5be6d685f47
hyohyo2 <hono.handball.0910@gmail.com>
2024-05-30 01:26:01 +0000

    投稿数表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,22 @@ class Book < ApplicationRecord
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day)}
+    scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+    scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+    scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+  
+  # 前日投稿比
+  def the_day_before
+    ((today_book.count / yesterday_book.count ) * 100).floor
+  end
+  
+  # 先週投稿数比
+  def the_week_before
+    ((this_week_book.count / last_week_book.count ) * 100).floor
+  end
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
 
