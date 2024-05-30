1b8f3139ef94bec2e1ed3fa48df2a30287bf4e44
hyohyo2 <hono.handball.0910@gmail.com>
2024-05-30 08:57:44 +0000

    7日間の投稿数一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books.page(params[:page]).per(5).reverse_order
-    @today_book = @books.created_today
-    @yesterday_book = @books.created_yesterday
-    @this_week_book = @books.created_this_week
-    @last_week_book = @books.created_last_week
+   
   end
 
   def index
